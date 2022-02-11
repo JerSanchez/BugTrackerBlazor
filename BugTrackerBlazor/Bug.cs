7f68f8e8ca1cbc894193b0c682f39e8cd4658944
jeraldsanchez <jeraldalipiosanchez@gmail.com>
2022-02-11 16:25:53 +0800

    Add project files.

---
+++ BugTrackerBlazor/Bug.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerBlazor
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [MinLength(10)]
+        [Required]
+        public string Description { get; set; }
+
+        [Range(1,5)]
+        [Required]
+        public int Priority { get; set; }
+    }
+}
---
+++ BugTrackerBlazor/Services/BugService.cs
@@ -0,0 +1,24 @@
+﻿using BugTrackerBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerBlazor.Services
+{
+    public class BugService : IBugService
+    {
+        private List<Bug> Bugs = new List<Bug>();
+
+        public void AddBug(Bug newBug)
+        {
+            newBug.Id = Bugs.Count + 1;
+            Bugs.Add(newBug);
+        }
+
+        public List<Bug> GetBugs()
+        {
+            return Bugs;
+        }
+    }
+}
